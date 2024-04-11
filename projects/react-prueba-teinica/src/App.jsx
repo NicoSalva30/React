88364ed27ea364d34c87b05b96bf5691cc8bb05e
NicoSalva30 <nicolas_salva1@hotmail.com>
2024-04-11 15:12:43 -0300

    terminando prueba con Hooks

--- projects/react-prueba-teinica/src/App.jsx
+++ projects/react-prueba-teinica/src/App.jsx
@@ -1,34 +1,15 @@
-import { useState, useEffect } from 'react'
 import './App.css'
-import { getRandomFact } from './service/facts.js'
+import { useCatFact } from './hooks/useCatFact.js'
+import { useCatImage } from './hooks/useCatImage.js'
 
 const CAT_IMG_URL = 'https://cataas.com'
-// const FIRST_CAT = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
 
 export function App () {
-  const [fact, setFact] = useState()
-  const [imageUrl, setImageUrl] = useState()
+  const { fact, getFact } = useCatFact()
+  const { imageUrl } = useCatImage({ fact })
 
-  useEffect(() => {
-    getRandomFact().then(setFact)
-  }, [])
-
-  useEffect(() => {
-    if (!fact) return
-    const firstWord = fact.split(' ', 3).join(' ')
-    console.log(firstWord)
-
-    fetch(`https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`)
-      .then(res => res.json())
-      .then(response => {
-        const { url } = response
-        console.log(response)
-        setImageUrl(url)
-      })
-  }, [fact])
-
-  const handleClick = () => {
-    getRandomFact()
+  const handleClick = async () => {
+    getFact()
   }
 
   return (
