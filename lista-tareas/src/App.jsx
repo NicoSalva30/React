1f838a4b5627b0eb2d0b50ab04a49c312370c0cf
NicoSalva30 <nicolas_salva1@hotmail.com>
2024-09-19 22:40:29 -0300

    arreglando img

--- lista-tareas/src/App.jsx
+++ lista-tareas/src/App.jsx
@@ -2,22 +2,18 @@ import './App.css'
 import LogoCocaCola from './imagenes/logo.png'
 import ListaDeTareas from './componentes/ListaDeTareas'
 
-
-function App() {
+function App () {
   return (
-    <div className="aplicacion-tareas">
+    <div className='aplicacion-tareas'>
       <div className='freecodecamp-logo-contenedor'>
-        <img 
-        src={LogoCocaCola} 
-        className='freecodecamp-logo' 
-        />  
-      </div>
-      <div className='tareas-lista-principal'>
-        <h1>Mis Tareas</h1>
-        <ListaDeTareas />
+        <img src={LogoCocaCola} className='freecodecamp-logo' alt='logo-cocacola' />
+        <div className='tareas-lista-principal'>
+          <h1>Mis Tareas</h1>
+          <ListaDeTareas />
+        </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
